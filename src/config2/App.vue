fdf74ea5715f4f3f963aa1713a2b28d255988e88
Edgar Rezende <edgar.rezende@mercadolivre.com>
2023-12-18 20:20:06 -0300

    updates

---
+++ src/config2/App.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>Config 2 @ Edgar</h1>
+</template>
+
+
+<script lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+export default {
+    setup() {
+        console.log("test");
+    }
+}
+
+</script>
+
+
+<style scoped>
+</style>
